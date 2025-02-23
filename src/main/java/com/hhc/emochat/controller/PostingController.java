a5fcb0f633c12bbea92a5938802c2b3c30ec7a71
HaHeonchan <gjscks0106@gmail.com>
2025-02-23 15:19:15 +0900

    [feat]: 글 세부 사항 조회 기능 추가

--- src/main/java/com/hhc/emochat/controller/PostingController.java
+++ src/main/java/com/hhc/emochat/controller/PostingController.java
@@ -2,12 +2,14 @@ package com.hhc.emochat.controller;
 
 import com.hhc.emochat.entity.PostEntity;
 import com.hhc.emochat.repository.PostRepository;
+import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PostingController {
@@ -37,5 +39,16 @@ public class PostingController {
         PostEntity post = new PostEntity(title, content, "me");
         postRepository.save(post);
         return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    String detail(Model model, @PathVariable Long id) {
+        Optional<PostEntity> post = postRepository.findById(id);
+        if (!post.isPresent()) {
+            return "redirect:/error";
+        }
+
+        model.addAttribute("post", post.get());
+        return "detail.html";
     }
 }
